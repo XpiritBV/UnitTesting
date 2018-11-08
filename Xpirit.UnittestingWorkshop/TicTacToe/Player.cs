7ddc1079ce730aaff3d4f8364b7e0ceba14da8d9
Marc Duiker <marcduiker@gmail.com>
2018-11-08 17:32:11 +0100

    Added results of kata 1

---
+++ Xpirit.UnittestingWorkshop/TicTacToe/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class Player
+    {
+        public Player(string name, char mark)
+        {
+            Name = name;
+            Mark = mark;
+        }
+
+        public int NumberOfMoves { get; private set; }
+
+        public string Name { get;}
+
+        public char Mark { get; }
+
+        public void Move()
+        {
+            NumberOfMoves++;
+        }
+    }
+}
