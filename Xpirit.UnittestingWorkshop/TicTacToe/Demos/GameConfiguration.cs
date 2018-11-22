835ceaec2ca04b27685aaed5385472f24c582380
Marc Duiker <marcduiker@gmail.com>
2018-11-22 15:41:50 +0100

    Added ACL example

---
+++ Xpirit.UnittestingWorkshop/TicTacToe/Demos/GameConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TicTacToe.Demos
+{
+    public class GameConfiguration
+    {
+        public string Version => Environment.GetEnvironmentVariable("GameVersion");
+
+        public string TempFolder => Environment.GetEnvironmentVariable("GameTempFolder");
+    }
+
+    public class GameConfigurationWithInterface
+    {
+        private readonly IEnvironment environment;
+
+        public GameConfigurationWithInterface(IEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        public string Version => environment.GetEnvironmentVariable("GameVersion");
+
+        public string TempFolder => environment.GetEnvironmentVariable("GameTempFolder");
+    }
+}
